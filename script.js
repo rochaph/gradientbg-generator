1a7469fa9440a9b40d02ade0622dc07346136c10
Pedro Rocha <pdhro99@gmail.com>
2020-04-03 00:51:50 -0400

    Versão 2

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var defaultColor1 = "#00ff00"
-var defaultColor2 = "#ff0000"
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let changeButton = document.querySelector(".change-color");
+let body = document.getElementById("gradient");
+let defaultColor1 = "#00ff00"
+let defaultColor2 = "#ff0000"
 
 css.textContent = body.style.background;
 
-window.addEventListener("load", startup, false);
+window.addEventListener("load", setColor(), false);
 
-
-function startup() {
-    color1.value = defaultColor1;
-    color2.value = defaultColor2;
+function changeColor() {
+    aux = color1.value;
+    color1.value = color2.value;
+    color2.value = aux;
     setColor();
 }
 
-function setColor () {
-    body.style.background = 
-    "linear-gradient(to right, "
-    + color1.value
-    + ", " 
-    + color2.value 
-    + ")";
-    
+function setColor() {
+    body.style.background =
+        "linear-gradient(to right, "
+        + color1.value
+        + ", "
+        + color2.value
+        + ")";
+
     css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setColor);
 
 color2.addEventListener("input", setColor);
+
+changeButton.addEventListener("click", changeColor);
